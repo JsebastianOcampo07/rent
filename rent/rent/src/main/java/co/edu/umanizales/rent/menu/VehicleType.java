1fce9b40201729154517bc7737f15839aedd9717
JsebastianOcampo07 <161963689+JsebastianOcampo07@users.noreply.github.com>
2024-11-21 15:11:43 -0500

    Delete rent directory

--- rent/rent/src/main/java/co/edu/umanizales/rent/menu/VehicleType.java
+++
@@ -1,7 +0,0 @@
-package co.edu.umanizales.rent.menu;
-
-public enum VehicleType {
-    CAR,
-    MOTORCYCLE,
-    VAN
-}
--- rent/src/main/java/co/edu/umanizales/rent/menu/VehicleMenu.java
+++
@@ -1,4 +0,0 @@
-package co.edu.umanizales.rent.menu;
-
-public class VehicleMenu {
-}
